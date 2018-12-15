c71fb88a071508ba9d5746b30cfdcbc454c52540
zimingwangEXP <2250606701@qq.com>
2018-12-15 17:42:51 +0800

    the basic  binary classification
    v1.0

---
+++ MinistSolution1.py
@@ -0,0 +1,69 @@
+from Tempotron import Tempotron
+from keras.datasets import mnist
+import numpy as np
+import scipy.io as scio
+from matplotlib import pyplot as plt
+import cv2
+
+
+def Encoding2():
+    return;
+
+
+# 只对灰度图进行编码
+def Encoding1(t_max, thred, img):
+    bck = [];
+    alpha = (np.exp(t_max) - 1) / thred;
+    for x in img:
+        for y in x:
+            bck.append([t_max - np.log(alpha * y + 1)])
+    return bck
+
+
+def LoadMinist():
+    global train_size, test_size
+    (X_train_multi, y_train_multi), (X_test_multi, y_test_multi) = mnist.load_data();
+    X_train_bin = []
+    y_train_bin = []
+    X_test_bin = []
+    y_test_bin = []
+    #将minist数据集中的1,7手写体单独拿出来，构造成一个二分类的数据集
+    for i in range(len(y_train_multi)):
+        if (y_train_multi[i] == 1 or y_train_multi[i] == 7):
+            X_train_bin.append(X_train_multi[i])
+            y_train_bin.append(y_train_multi[i]==1)
+    for i in range(len(y_test_multi)):
+        if (y_test_multi[i] == 1 or y_test_multi[i] == 7):
+            X_test_bin.append(X_test_multi[i])
+            y_test_bin.append(y_test_multi[i]==1)
+    #将二分类数据集规模减小
+    X_train_small = X_train_bin[:train_size]
+    y_train_small = y_train_bin[:train_size]
+    X_test_small = X_test_bin[:test_size]
+    y_test_small = y_test_bin[:test_size]
+    train_spike = []
+    test_spike = []
+    for i in range(train_size):
+        train_spike.append((Encoding1(500, 255, X_train_small[i]), y_train_small[i]))
+    for i in range(test_size):
+        test_spike.append((Encoding1(500, 255, X_test_small[i]), y_test_small[i]))
+    return  (train_spike,test_spike)
+
+if __name__ == '__main__':
+    global train_size, test_size
+    train_size = 10
+    test_size = 2
+    np.random.seed(0);
+    efficacies = 1.8 * np.random.random(28 * 28) - 0.50
+    (train_spike, test_spike)=LoadMinist()
+    work=Tempotron(efficacies,0,10,2.5,1,3);
+    work.Train(train_spike)
+
+    """
+    cnt=0;
+    for i in range(test_size):
+      if((work.ComputeMembranePotential(test_spike[i][0],work.ComputeTmax1(test_spike[i][0]))>1)==test_spike[i][1]):
+          cnt+=1;
+    print('accucy at test data is %f%%' %(cnt/test_size));
+    """
+
---
+++ Tempotron.py
@@ -0,0 +1,120 @@
+"""
+相关代码注释请阅读相应的类设计文档
+实现参考了 https://github.com/dieuwkehupkes/Tempotron
+
+"""
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+class Tempotron:
+
+    def __init__(self, w, v_rest, tau, tau_s, thred_hold, max_iter=1, alpha=0.1):
+        self.w = w;
+        self.v_rest = v_rest;
+        self.tau = tau;
+        self.tau_s = tau_s;
+        self.thred_hold = thred_hold;
+        self.max_iter = max_iter;
+        self.alpha = alpha;
+        self.v_0 = self.ComputeV_0();
+        self.lamb = 0.0001 / self.v_0;
+        #self.lamb=1;
+
+    def K(self, t, ti, v_0=1):
+        if (t < ti):
+            return 0;
+        return v_0 * (np.exp(-(t - ti) / self.tau) - np.exp(-(t - ti) / self.tau_s));
+
+    def ComputeV_0(self):
+        t_max = (self.tau * self.tau_s * np.log(self.tau / self.tau_s)) / (self.tau - self.tau_s);
+        v_max = self.K(t_max, 0);
+        return 1.0 / v_max;
+
+    def ComputeContributions(self, spike_input, t):
+        sz = len(spike_input);
+        ans = np.zeros(sz);
+        for i in range(sz):
+            for spike_time in spike_input[i]:
+                ans[i] += self.K(t, spike_time, self.v_0);
+        return ans;
+
+    def ComputeMembranePotential(self, spike_input, t):
+        res=self.ComputeContributions(spike_input, t);
+        #print(res)
+        res=res* self.w;
+        res=res.sum()+self.v_rest;
+        return res
+
+    def GetOutput(self, t_start, t_end, spike_input, interval=0.1):
+        t = list();
+        v = list();
+        for i in np.arange(t_start, t_end, interval):
+            t.append(i);
+            #print(type(self.ComputeMembranePotential(spike_input, i)));
+            v.append(self.ComputeMembranePotential(spike_input, i));
+        return (t, v);
+
+
+    def PlotVT(self, t_start, t_end, spike_input, interval=0.1):
+        (t, v) = self.GetOutput(t_start, t_end, spike_input, interval);
+        plt.xlabel('Time (ms)');
+        plt.ylabel('V(t)');
+        ymax = max(max(v), self.thred_hold) + 0.1;
+        ymin = min(min(v), -self.thred_hold) - 0.1;
+        plt.axis([0,500,ymin,ymax]);
+        plt.axhline(y=self.thred_hold, linestyle='--', color='k');
+        plot = plt.plot(t, v);
+        #plt.show()
+
+    # ComputeTmax应该有两种很多求解的方式
+    # 1.根据参考资料导数等于0来求解析解解
+    # 2.离散求近似解
+
+
+    def ComputeTmax1(self, input_spike):
+        log_tts = np.log(self.tau / self.tau_s)
+        tmp = [(time, id) for id in range(len(input_spike)) for time in input_spike[id]]
+        tmp.sort()
+        spikes = [(one[0], self.w[one[1]]) for one in tmp]
+        times = np.array([spike[0] for spike in spikes])
+        weights = np.array([spike[1] for spike in spikes])
+        sum_tau = (weights * np.exp(times / self.tau)).cumsum()
+        sum_tau_s = (weights * np.exp(times / self.tau_s)).cumsum()
+        div = sum_tau_s / sum_tau
+        boundary_cases = div < 0
+        div[boundary_cases] = 10
+        tmax_list = self.tau * self.tau_s * (log_tts + np.log(div)) / (self.tau - self.tau_s)
+        tmax_list[boundary_cases] = times[boundary_cases]
+        vmax_list = np.array([self.ComputeMembranePotential(input_spike,t) for t in tmax_list])
+        tmax = tmax_list[vmax_list.argmax()]
+        return tmax
+    def ComputeTmax2(self,input_spike):
+        return ;
+
+    # output表示是否发放脉冲
+    def AdaptWeight(self, input, label):
+        t_max = self.ComputeTmax1(input);
+        v_max = self.ComputeMembranePotential(input, t_max);
+        if ((v_max > self.thred_hold) == label):
+            return False;
+        dw = self.ComputeContributions(input, t_max) * self.alpha * self.lamb;
+        if (label == 1):
+            self.w += dw;
+        else:
+            self.w -= dw;
+        return True;
+
+    def Train(self, train_data):
+        for i in range(self.max_iter):
+            cnt = 0;
+            for (data, label) in np.random.permutation(train_data):
+                t_max = self.ComputeTmax1(data);
+                v_max = self.ComputeMembranePotential(data, t_max);
+                if ((v_max >= self.thred_hold) == label):
+                    cnt += 1
+            print('%d/%d epoches,accuracy is %f%%' % (i, self.max_iter, 100*cnt / len(train_data)))
+            for (data, label) in np.random.permutation(train_data):
+                if(self.AdaptWeight(data, label)):
+                    print(label)
+
